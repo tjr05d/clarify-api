7424883f5566c7d21b7b82b70076a2497e5e37c3
tjr05d <tjr05d@my.fsu.edu>
2017-09-25 09:31:15 -0400

    removes devise, rolls own token auth

--- Gemfile
+++ Gemfile
@@ -13,14 +13,13 @@ gem 'pg'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 #auth
-gem 'devise'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'simple_token_authentication'
 # gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  # devise_for :users
   resources :clarifications
 
-  resources :sessions, only: [:create, :destroy]
+  namespace :api do
+    post   'login'   => 'sessions#create'
+    delete 'logout'  => 'sessions#destroy'
+    get    'verify'  => 'sessions#verify_access_token'
+    resources :users, param: :access_token
+    resources :password_resets, only: [:new, :create, :edit, :update]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
